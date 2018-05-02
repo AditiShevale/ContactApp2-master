f8963cb4ae3fd2217078aadbad9e9cf4faebc539
aditi.shevale999@gmail.com <aditi.shevale999@gmail.com>
2018-05-02 17:27:47 +0530

    Solved

---
+++ app/src/main/java/com/example/aditi/contactapp2/Pojo/FavAdapter.java
@@ -0,0 +1,15 @@
+package com.example.aditi.contactapp2.Pojo;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.aditi.contactapp2.R;
+
+import java.util.List;
+
+public class FavAdapter{
+}
